5f431c540b0b5a9ac561856c391af3c885ca9986
Jucilene Barros Silva <barrossilvajucilene@gmail.com>
2020-07-27 10:56:09 -0300

    salvando-minhas-alteraçoes

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,59 +1,66 @@
-import React, { useState } from 'react'
-import logo from '../../img/back.jpg'
+import React, { useState } from 'react';
+import logo from '../../img/back.jpg';
 import './Login.css';
 import { TextField, Button, Link } from '@material-ui/core';
 
 export const Login = () => {
-    const [email, setEmail] = useState();
-    const [senha, setSenha] = useState();
+  const [email, setEmail] = useState();
+  const [senha, setSenha] = useState();
 
-   return (
+  return (
     <div className="container">
       <div className="container-logo">
-          <img src={logo} alt="Logomarca"></img>
-    </div>
+        <img src={logo} alt="Logomarca"></img>
+      </div>
       <div className="container-form">
-      <form noValidate autoComplete="off">      
-        <TextField variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="E-mail"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              value={setEmail}
-        />
-        <TextField variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Senha"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              value={setSenha}
-        />
-        <Button type="submit"
-              fullWidth
-              variant="contained"
-              color="primary" onClick={() => { alert('clicado') }}>Acessar
-        </Button>
-        <div className="container-inf">
-              <Link href="#" variant="body2">
-                  Esqueceu a senha?
-              </Link>         
-              <Link href="#" variant="body2">
-                  {"Não tem uma conta? Cadastre-se!"}
-              </Link>     
-        </div>                     
-      </form>
+        <form noValidate autoComplete="off">
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="E-mail"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={setEmail}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Senha"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={setSenha}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            onClick={() => {
+              alert('clicado');
+            }}
+          >
+            Acessar
+          </Button>
+          <div className="container-inf">
+            <Link href="#" variant="body2">
+              Esqueceu a senha?
+            </Link>
+            <Link href="#" variant="body2">
+              {'Não tem uma conta? Cadastre-se!'}
+            </Link>
+          </div>
+        </form>
       </div>
     </div>
   );
-
-}
+};
 
 export default Login;
