7f4cef396391b794b2ceaf47f918e6be3e0280fe
Milena Ferraz <milenaferraz@gmail.com>
2020-07-27 10:37:24 -0300

    refatoraçao-login

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,59 +1,54 @@
 import React, { useState } from 'react'
-import logo from '../../img/back.jpg'
+import logo from '../../img/logo.svg'
 import './Login.css';
-import { TextField, Button, Link } from '@material-ui/core';
+import { TextField, Link } from '@material-ui/core';
+// import Firebase from 'Firebase';
+import BtButton from '../../Components/Button.js';
+import Input from '../../Components/Input.js';
+import Checkbox from '../../Components/Checkbox.js';
 
 export const Login = () => {
     const [email, setEmail] = useState();
-    const [senha, setSenha] = useState();
+    const [password, setSenha] = useState();
+
+    // loginEmailAndPass(email, password) {
+    //     firebase
+    //       .auth()
+    //       .signInWithEmailAndPassword(email, password)
+    //       .then(() => {
+    //         window.location.href = '#feed';
+    //       })
+    //       .catch((error) => {
+    //         if (error.code === 'auth/wrong-password') {
+    //           return 'Senha incorreta!';
+    //         }
+    //         if (error.code === 'auth/user-not-found') {
+    //           return 'E-mail não localizado!';
+    //         }
+    //         if (error.code === 'auth/invalid-email') {
+    //           return 'E-mail invalido!';
+    //         }
+    //         return `Codigo de error: ${error.code}`;
+    //       });
+    //   };      
 
    return (
     <div className="container">
       <div className="container-logo">
-          <img src={logo} alt="Logomarca"></img>
+          <img className="logo" src={logo} alt="Logomarca"></img>
     </div>
       <div className="container-form">
-      <form noValidate autoComplete="off">      
-        <TextField variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="E-mail"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              value={setEmail}
-        />
-        <TextField variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Senha"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              value={setSenha}
-        />
-        <Button type="submit"
-              fullWidth
-              variant="contained"
-              color="primary" onClick={() => { alert('clicado') }}>Acessar
-        </Button>
-        <div className="container-inf">
-              <Link href="#" variant="body2">
-                  Esqueceu a senha?
-              </Link>         
-              <Link href="#" variant="body2">
-                  {"Não tem uma conta? Cadastre-se!"}
-              </Link>     
+      <form noValidate autoComplete="off"> 
+            <Input id="email" name="email" label="E-mail" type="email" setEmail={setEmail}/>
+            <Input id="password" name="password" label="Senha" type="password" setEmail={setSenha}/>
+            <BtButton name="Logar" />
+            <div className="container-inf">
+            <Checkbox name="Esqueceu a senha?"/>
+            <Checkbox name="Não tem uma conta? Cadastre-se!" />
         </div>                     
       </form>
       </div>
     </div>
   );
-
 }
-
 export default Login;
---
+++ src/components/Input.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { TextField } from '@material-ui/core';
+
+const Input = (props) => {
+    return (
+        <TextField variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id= {props.id}
+              label= {props.label}
+              name= {props.name}
+              autoComplete= {props.type}
+              autoFocus
+              value= {props.setEmail}
+        />
+    )
+}
+
+export default Input;
