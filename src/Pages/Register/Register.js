e032afe1f1411f8979eb82992c65d8e124dd05cd
Jucilene Barros Silva <barrossilvajucilene@gmail.com>
2020-07-28 14:33:15 -0300

    tela de cadastro funcionando

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -4,7 +4,8 @@ import '../Login/Login.css';
 import BtButton from '../../Components/Button.js';
 import Input from '../../Components/Input.js';
 import LinkRel from '../../Components/Link'
-import Checkbox from '../../Components/Checkbox.js';
+// import Checkbox from '../../Components/Checkbox.js';
+//import firebaseConfig from '../../Firebase';
 // import { FormControl } from '@material-ui/core';
 
 export const Register = () => {
@@ -14,23 +15,24 @@ export const Register = () => {
   const [salao, setSalao] = useState();
   const [cozinha, setCozinha] = useState();
   
-
-  function register(uid, userName, occupation) {
-    const db = firebase.firestore();
-    const authUid = firebase.auth().currentUser.uid;
-    db.collection('employee').doc(authUid).set({
-      userUid: uid,
-      nickName: userName,
-      occupation: occupation,
+  // function registerUser(uid, userName, occupation) {
+  //   const db = firebase.firestore();
+  //   const authUid = firebase.auth().currentUser.uid;
+  //   db.collection('employee').doc(authUid).set({
+  //     userUid: uid,
+  //     nickName: userName,
+  //     occupation: occupation,
       
-    });
-  }
+  //   });
+  // }
   
-  function createUser(email, userName, occupation) {
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-      .then(() => firebase.auth().currentUser.updateProfile({ displayName: `${userName}`, occupation: `${occupation}` }))
-      .then(() => { const uid = firebase.auth().currentUser.uid; register(uid, userName, occupation); })
-      .catch(error => `Codígo de error: ${error.message}`);
+  // function createUser(email, userName, occupation) {
+  //   firebase.auth().createUserWithEmailAndPassword(email, password)
+  //     .then(() => firebase.auth().currentUser.updateProfile({ displayName: `${userName}`, occupation: `${occupation}` }))
+  //     .then(() => { const uid = firebase.auth().currentUser.uid; register(uid, userName, occupation); })
+  //     .catch(error => `Codígo de error: ${error.message}`);
+
+  // }
   
   return (
     <div className ="container">
@@ -42,8 +44,8 @@ export const Register = () => {
           <Input id="name" name="name" label="Name" type="text" setName={setName}/>
           <Input id="email" name="email" label="E-mail" type="email" setEmail={setEmail}/>
           <Input id="password" name="password" label="Senha" type="password" setSenha={setSenha}/>
-          <Checkbox label1="Salão" value1="setSalao" name1="Cozinha" onChange= {setSalao} />
-          <Checkbox label2="Cozinha" value2="setCozinha" name2="Salão" onChange= {setCozinha} />       
+          {/* <Checkbox label1="Salão" value1="setSalao" name1="Cozinha" onChange= {setSalao} />
+          <Checkbox label2="Cozinha" value2="setCozinha" name2="Salão" onChange= {setCozinha} />        */}
           <BtButton name="Cadastrar" />
           <div className="container-inf">
                 <LinkRel name="Tem uma conta? Conecte-se!" />
@@ -53,6 +55,6 @@ export const Register = () => {
     </div>
   )
 
-}
+};
 
-export default Register
+export default Register;
