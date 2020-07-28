13c8e1059666ee7ce67954292e7126a11c82e1ec
Milena Ferraz <milenaferraz@gmail.com>
2020-07-28 14:37:51 -0300

    sem-conf-firebase

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -3,29 +3,8 @@ import './Login.css';
 import FormLogin from '../../Components/FormLogin.js';
 import Logo from '../../Components/Logo.js';
 
-// import Firebase from 'Firebase';
 
 export const Login = () => {
-	// loginEmailAndPass(email, password) {
-	//     firebase
-	//       .auth()
-	//       .signInWithEmailAndPassword(email, password)
-	//       .then(() => {
-	//         window.location.href = '#feed';
-	//       })
-	//       .catch((error) => {
-	//         if (error.code === 'auth/wrong-password') {
-	//           return 'Senha incorreta!';
-	//         }
-	//         if (error.code === 'auth/user-not-found') {
-	//           return 'E-mail não localizado!';
-	//         }
-	//         if (error.code === 'auth/invalid-email') {
-	//           return 'E-mail invalido!';
-	//         }
-	//         return `Codigo de error: ${error.code}`;
-	//       });
-	//   };
 
 	return (
 		<div className="container">
@@ -38,4 +17,6 @@ export const Login = () => {
 		</div>
 	);
 };
+
+
 export default Login;
--- src/components/FormLogin.js
+++ src/components/FormLogin.js
@@ -3,16 +3,17 @@ import BtButton from '../../src/Components/Button.js';
 import Input from '../../src/Components/Input.js';
 import Checkbox from '../../src/Components/Checkbox.js';
 
+
 const FormLogin = () => {
     const [email, setEmail] = useState();
-    const [password, setSenha] = useState();
-
-	return (
+		const [password, setSenha] = useState();
+		
+		return (
 		<div>
 			<form noValidate autoComplete="off">
-				<Input id="email" name="email" label="E-mail" type="email" setEmail={setEmail} />
+				<Input id="email" name="email" label="E-mail" type="email" setEmail={setEmail}  />
 				<Input id="password" name="password" label="Senha" type="password" setEmail={setSenha} />
-				<BtButton name="Logar" />
+				<BtButton name="Acessar" />
 				<div className="container-inf">
 					<Checkbox name="Esqueceu a senha?" />
 					<Checkbox name="Não tem uma conta? Cadastre-se!" />
