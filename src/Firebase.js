b35cb1231f519c9cce276cee3d463e2dcf76bb5c
Jucilene Barros Silva <barrossilvajucilene@gmail.com>
2020-07-31 12:25:27 -0300

    configurando o firebase

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,16 +1,16 @@
-// import app from 'firebase/app';
-// import 'firebase/database';
-// import 'firebase/auth';
+import * as firebase from 'firebase';
+import 'firebase/database';
+import 'firebase/auth';
 
+const firebaseConfig = {
+    apiKey: "AIzaSyC5jeSWsQC5iUB3pocuJ6eUh1OugevKwKM",
+    authDomain: "burguer-queen-a8030.firebaseapp.com",
+    databaseURL: "https://burguer-queen-a8030.firebaseio.com",
+    projectId: "burguer-queen-a8030",
+    storageBucket: "burguer-queen-a8030.appspot.com",
+    messagingSenderId: "828621373470",
+    appId: "1:828621373470:web:4075c305e3c7118aad2e0f"
+  };
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyC5jeSWsQC5iUB3pocuJ6eUh1OugevKwKM",
-//     authDomain: "burguer-queen-a8030.firebaseapp.com",
-//     databaseURL: "https://burguer-queen-a8030.firebaseio.com",
-//     projectId: "burguer-queen-a8030",
-//     storageBucket: "burguer-queen-a8030.appspot.com",
-//     messagingSenderId: "828621373470",
-//     appId: "1:828621373470:web:4075c305e3c7118aad2e0f"
-//   };
-
-//   export default firebaseConfig;
+  firebase.initializeApp(firebaseConfig);
+  export default firebase;
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -5,8 +5,9 @@ import BtButton from '../../Components/Button.js';
 import Input from '../../Components/Input.js';
 import LinkRel from '../../Components/Link'
 import BtRadio from '../../Components/BtRadio.js';
-//import firebaseConfig from '../../Firebase';
+import firebase from '../../Firebase';
 // import { FormControl } from '@material-ui/core';
+console.log(firebase);
 
 export const Register = () => {
   const [name, setName] = useState();
@@ -14,38 +15,39 @@ export const Register = () => {
   const [password, setSenha ] = useState();
   const [salao, setSalao] = useState();
   const [cozinha, setCozinha] = useState();
+  const [occupation, setOccupation] = useState();
   
-  // function registerUser(uid, userName, occupation) {
-  //   const db = firebase.firestore();
-  //   const authUid = firebase.auth().currentUser.uid;
-  //   db.collection('employee').doc(authUid).set({
-  //     userUid: uid,
-  //     nickName: userName,
-  //     occupation: occupation,
-      
-  //   });
-  // }
   
-  // function createUser(email, userName, occupation) {
-  //   firebase.auth().createUserWithEmailAndPassword(email, password)
-  //     .then(() => firebase.auth().currentUser.updateProfile({ displayName: `${userName}`, occupation: `${occupation}` }))
-  //     .then(() => { const uid = firebase.auth().currentUser.uid; register(uid, userName, occupation); })
-  //     .catch(error => `Codígo de error: ${error.message}`);
+  function registerUser(uid, userName, occupation) {
+    const db = firebase.firestore();
+    const authUid = firebase.auth().currentUser.uid;
+    db.collection('employee').doc(authUid).set({
+      userUid: uid,
+      nickName: userName,
+      occupation: occupation,
+    });
+  }
+  
+  function createUser(email, userName, occupation) {
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then(() => firebase.auth().currentUser.updateProfile({ displayName: `${userName}`, occupation: `${occupation}` }))
+      .then(() => { const uid = firebase.auth().currentUser.uid; registerUser(uid, userName, occupation); })
+      .catch(error => `Codígo de error: ${error.message}`);
 
-  // }
+  }
   
   return (
     <div className ="container">
       <div className= "container-logo">
         <img className="logo" src={logo} alt="Logomarca"></img>
       </div>
-      <div className="container-form">
+      <div className="container-form"> 
         <form noValidate autoComplete="off">
-          <Input id="name" name="name" label="Name" type="text" setName={setName}/>
-          <Input id="email" name="email" label="E-mail" type="email" setEmail={setEmail}/>
-          <Input id="password" name="password" label="Senha" type="password" setSenha={setSenha}/>
-          <BtRadio value1="setSalao" value2="setCozinha" label1="Salão" label2="Cozinha" onChange= {setSalao} /> 
-          <BtButton name="Cadastrar" />
+          <Input id="name" name="name" label="Name" type="text" onChange={e=> console.log(setName(e.target.value))}/>
+          <Input id="email" name="email" label="E-mail" type="email" onChange={e=> console.log(setEmail(e.target.value))} />
+          <Input id="password" name="password" label="Senha" type="password" onChange={e=> console.log(setSenha(e.target.value))}/>
+          <BtRadio value1="setSalao" value2="setCozinha" label1="Salão" label2="Cozinha" onChange={e=> console.log(setOccupation(e.target.value))}/> 
+          <BtButton name="Cadastrar" onClick={e=> console.log(e.target.value)}/>
           <div className="container-inf">
                 <LinkRel name="Tem uma conta? Conecte-se!" />
               </div>  
