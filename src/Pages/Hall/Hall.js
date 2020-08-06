ad9ebc9776eea119c115ee4516755dafe2f760c9
Jucilene Barros Silva <barrossilvajucilene@gmail.com>
2020-08-06 18:32:52 -0300

    adicionando o input da mesa e nome do cliente

--- src/Pages/Hall/Hall.js
+++ src/Pages/Hall/Hall.js
@@ -4,13 +4,15 @@ import Button from '../../Components/Button.js';
 import 'firebase/firestore';
 import firebase from '../../Firebase.js';
 import './Hall.css';
-
+import Input from '../../Components/Input.js';
 
 
 function Hall() {
   const [menu, setMenu] = useState([]);
   const [breakfast, setBreakfast] = useState(false);
   const [meal, setMeal] = useState(false);
+  const [clientName, setClientName] = useState('');
+  const [tableNumber, setTableNumber] = useState('');
 
   useEffect(() => {
     firebase.firestore().collection('menu')   
@@ -25,25 +27,53 @@ function openBreakfast(){
   setBreakfast(true)
   setMeal(false)
 }
+
 function openMeal(){
   setBreakfast(false)
   setMeal(true)
 }
 
+const handleChange = (event) => {
+  setClientName(event.target.value)
+  setTableNumber(event.target.value);
+};
 
   return (
 
     <div>
+      <main></main>
       <Header />
-      <h1>Salão</h1>
-      
-      
-          {breakfast && menu.filter((item)=>item.breakfast=== true).map((item)=><div className='card'>{item.item} {item.value} <br /></div>)}
-          {meal && menu.filter((item)=>item.breakfast=== false).map((item)=><div className='card'>{item.item} {item.value} <br /></div>)}
-          
+      <h1>Salão</h1>  
+      <Input 
+      autoFocus="true"
+      id="clientName"
+      label="Nome do cliente "
+      name="clientName"
+      type="text"
+      value={clientName}
+      onChange={handleChange}
+      setValue={setClientName}
+      />
 
+      <Input 
+      id="tableNumber"
+      label="Número da mesa"
+      name="tableNumber"
+      type="text"
+      autoComplete={tableNumber}
+      value={tableNumber}
+      onChange={handleChange}
+      setValue={setTableNumber}
+      />
+            
       <Button name='Café da manhã' onClick={openBreakfast}/>
       <Button name='Refeição' onClick={openMeal}/>
+      
+      {breakfast && menu.filter((item)=>item.breakfast=== true).map((item)=><div className='card'>{item.item} {item.value} <br /></div>)}
+      {meal && menu.filter((item)=>item.breakfast=== false).map((item)=><div className='card'>{item.item} {item.value} <br /></div>)}
+      
+      
+      
     </div>
   )
 }
